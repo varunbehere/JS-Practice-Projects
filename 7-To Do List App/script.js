5d399c6e9b09629dd46ea1f2860ea27e88e1dfd4
varunbehere <varunbehere@gmail.com>
2024-03-30 23:50:54 +0530

    Done with the project

--- 7-To Do List App/script.js
+++ 7-To Do List App/script.js
@@ -1,17 +1,23 @@
-const inputField = document.getElementById('inputField')
-const addBtn = document.getElementById('addBtn')
-const ul = document.querySelector('ul')
+const inputField = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const ul = document.querySelector('#todo-list');
+const ulondone = document.getElementById('done-list');
 
-addBtn.addEventListener('click',function(){
-    let dataValue = inputField.value
-    let currDate = new Date()
-    console.log(currDate.toLocaleDateString() + ' ' + currDate.getHours() + ":" + currDate.getMinutes());
-    let newDataitem = document.createElement('li')
-    newDataitem.innerText = dataValue
-    ul.appendChild(newDataitem)
-})
+addBtn.addEventListener('click', function() {
+    let dataValue = inputField.value;
+    let currDate = new Date();
+    let currdatetime = currDate.toLocaleDateString() + ' ' + currDate.getHours() + ":" + currDate.getMinutes();
+    let newDataitem = document.createElement('li');
+    newDataitem.innerText = `${currdatetime} - ${dataValue}`; 
+    ul.appendChild(newDataitem);
 
-
-console.log(inputField)
-console.log(addBtn)
-console.log(ul);
+    newDataitem.addEventListener('click', function() {
+        let newElementonDone = newDataitem.cloneNode(true); 
+        let currDate = new Date();
+        let currdatetime = currDate.toLocaleDateString() + ' ' + currDate.getHours() + ":" + currDate.getMinutes();
+        newElementonDone.innerText = `${currdatetime} - ${dataValue}`; 
+        newElementonDone.classList.add('done-list');
+        ulondone.appendChild(newElementonDone);
+        newDataitem.remove();
+    });
+});
